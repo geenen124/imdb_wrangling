f19dec2c5d60542c9c4a0d1943f5c2adaa0a9107
Alex Geenen <ageenen@pm.me>
2021-06-08 14:22:00 +0200

    Dedupe Movie Matches

--- parse_data.py
+++ parse_data.py
@@ -2,6 +2,7 @@ import json
 import pathlib
 
 import pandas as pd
+from Levenshtein import distance as lev
 
 
 FILE_DIR = pathlib.Path(__file__).parent.absolute()
@@ -27,7 +28,6 @@ if __name__ == "__main__":
 
     # initial genre prep
     second_v_df["genres"] = second_v_df["genre"].apply(lambda x: [] if pd.isna(x) else [x])
-    print(second_v_df)
 
     # Then load the movie vertices - Movies are what these 2 datasets have in common, so that's what we'll be joining on
     first_m_df = pd.read_csv(FIRST_DSET_DIR / "movies.csv")
@@ -43,26 +43,95 @@ if __name__ == "__main__":
     first_m_df = first_m_df.drop(columns=genres)
 
     # Join the two together
+    # TODO: This is broken!! Can't just merge on the movie title, as these aren't unique
     in_common = pd.merge(first_m_df, second_v_df, left_on=" movie title ", right_on="title")
+
+    # first replace descriptions if they're useful
+    no_descrip_idx = in_common.description == "No overview found."
+    in_common.loc[no_descrip_idx, "description"] = in_common[no_descrip_idx][" description"]
+
+    # deduplicate titles based on description Levenshtein distance
+    in_common["distance"] = in_common.apply(lambda x: lev(str(x["description"]), str(x[" description"])), axis=1)
+    to_keep_idx = in_common.groupby("title")["distance"].transform(min) == in_common["distance"]
+    should_be = in_common[to_keep_idx]
+    # and then on release year (account for rereleases)
+    remaining_conflict_ids = should_be.groupby("movie_id").size().sort_values(ascending=False)
+    remaining_conflict_ids = remaining_conflict_ids[remaining_conflict_ids > 1].index.tolist()
+
+    remaining_conflict = should_be[should_be.movie_id.isin(remaining_conflict_ids)]
+
+    r_year = remaining_conflict[" release date "].apply(lambda x: int(str(x)[-4:]))
+    start_year = remaining_conflict["released"].apply(lambda x: 3000 if pd.isna(x) else int(str(x)[:4]))
+    end_year = remaining_conflict["released"].apply(lambda x: 0 if pd.isna(x) else int(str(x)[-4:]))
+
+    resolved = remaining_conflict[((r_year >= start_year) & (r_year <= end_year))]
+
+    in_common = pd.concat([
+        should_be[~should_be.movie_id.isin(remaining_conflict_ids)],
+        resolved
+    ])
 
     # combine the columns!
     in_common["genres"] = in_common.apply(lambda x: list(set(x["genres_x"] + x["genres_y"])), axis=1)
 
     # Since dataset 2 is bigger, we can fold dataset 1 in. This means backfilling fields correctly as follows:
+    in_common["tagline"] = in_common[["tagline", " tagline"]].bfill(axis=1).iloc[:, 0]
+    in_common["title"] = in_common[["title", " movie title "]].bfill(axis=1).iloc[:, 0]
+    in_common["description"] = in_common[["description", " description"]].bfill(axis=1).iloc[:, 0]
+    in_common["studio"] = in_common[["studio", " studio "]].bfill(axis=1).iloc[:, 0]
+    in_common["IMDb URL"] = in_common[" IMDb URL "]
 
-    # next load the users in
-    # and do the user mappings here too!
+    # TODO: Figure out something useful for release dates - python/pandas parsers really don't like the (non-unix?) timestamp from the larger set
+
+    # drop remaining columns
+    in_common.drop(columns=[" tagline", " movie title ", " description", " studio ", " IMDb URL ", "genres_x", "genres_y", " unknown ", " release date ", " video release date "])
+
+    # use the same formatting for the remainder and
+    m_remainder = first_m_df[~first_m_df.movie_id.isin(in_common.movie_id)].copy().reset_index(drop=True)
+    m_remainder["type"] = "Movie"
+    m_remainder.rename(columns={
+        " tagline": "tagline",
+        " movie title ": "title",
+        " description": "description",
+        " studio ": "studio",
+        " IMDb URL ": "IMDb URL",
+    })
+    # again drop remaining columns
+    m_remainder.drop(columns=[" tagline", " movie title ", " description", " studio ", " IMDb URL ", " unknown ", " release date ", " video release date "])
 
     # we also need to create unique keys for each of the movies that weren't linked to the bigger dataset
-    # ...and also for the Users
+    start = len(second_v_df)
+    end = start + len(m_remainder)
+    m_remainder["_key"] = list(range(start, end))
+
+    # now combine all of the vertices
+    v_remainder = second_v_df[~second_v_df._key.isin(in_common._key)]
+    v_df = pd.concat([v_remainder, in_common, m_remainder])
+
+    # next load the users in
+    users_df = pd.read_csv(FIRST_DSET_DIR / "users.csv")
+
+    # add the mappings
+    users_df["type"] = "User"
+    # and also update the keys for the Users
+    start = end
+    end = start + len(users_df)
+    users_df["_key"] = list(range(start, end))
 
     # now that we have the updated ids - we can load in the ratings from the first dataset
+    ratings_df = pd.read_csv(FIRST_DSET_DIR / "ratings.csv")
 
-    # Join the ratings to the movies
+    l_rate_df = ratings_df.merge(v_df[["movie_id", "_key"]], how="left", left_on="item_id", right_on="movie_id")
+    l_rate_df["_from"] = l_rate_df["_key"]
+    l_rate_df = l_rate_df[["user_id", "Rating", "Timestamp", "_from"]]
+    full_rate_df = pd.merge(users_df[["user_id", "_key"]], l_rate_df, on="user_id")
+
+
+    # And join the ratings to the movies + users
     # next join these to the users
     # Replace the _from and _to key fields with the updated ids
 
-    # Now we should be good to go!
+    # Then add these ratings to the next ratings dataset
 
     # TODO: Add edges to the various genres
     # Persist the data
